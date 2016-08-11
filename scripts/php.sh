82098285b005ea246ed74d0d147d54bfe02b6668
david hill <david@davidhill.io>
2016-08-11 18:18:54 -0400

    added repo..shh.

--- scripts/php.sh
+++ scripts/php.sh
@@ -2,12 +2,16 @@
 
 echo "------ Installing Php and Friends ------"
 
+echo 'deb http://packages.dotdeb.org jessie all' | sudo tee --append /etc/apt/sources.list > /dev/null
+wget https://www.dotdeb.org/dotdeb.gpg
+sudo apt-key add dotdeb.gpg
+
 sudo apt-get update
 sudo apt-get install -y php7.0-cli php7.0-fpm php7.0-mysql php7.0-sqlite php7.0-curl php7.0-gd php7.0-gmp php7.0-mcrypt php7.0-xdebug php7.0-memcached php7.0-imagick php7.0-intl
 
 if [[ ! -f "/etc/php5/mods-available/xdebug.ini" ]]; then
 
-    cat > /etc/php5/mods-available/xdebug.ini << EOF 
+    cat > /etc/php/7.0/mods-available/xdebug.ini << EOF 
 zend_extension=$(find /usr/lib/php5 -name xdebug.so)
 xdebug.remote_enable = 1
 xdebug.remote_connect_back = 1
